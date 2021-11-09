fe6b6038291f47732ea7862ba7798b06f80e58b7
duongdvph14918 <duongdvph14918@fpt.edu.vn>
2021-11-09 23:33:59 +0700

    duong

--- DUAN1/src/MODEL/BanHang.java
+++ DUAN1/src/MODEL/BanHang.java
@@ -3,12 +3,12 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package VIEW;
+package MODEL;
 
 /**
  *
  * @author Admin
  */
-public class NewClass {
+public class BanHang {
     
 }
